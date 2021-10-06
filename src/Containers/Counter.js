b3dc78f67a48ec3e0be2b4ced04faf7332fdfa56
Tam Pham <tampham47@live.com>
2021-10-07 00:43:28 +0700

    update airdrop stats typo

--- src/Containers/Counter.js
+++ src/Containers/Counter.js
@@ -45,30 +45,30 @@ const Main = styled.div`
 export const Counter = () => {
   const [total, setTotal] = useState(9655);
 
-  // const getTotal = async () => {
-  //   const t = await fetch(
-  //     "https://api.solareum.app/airdrops/count?type=airdrop"
-  //   )
-  //     .then((resp) => {
-  //       return resp.text();
-  //     })
-  //     .catch(() => {
-  //       setTotal("1000");
-  //     });
-  //   setTotal(t);
-  // };
+  const getTotal = async () => {
+    const t = await fetch(
+      "https://api.solareum.app/airdrops/count?type=airdrop"
+    )
+      .then((resp) => {
+        return resp.text();
+      })
+      .catch(() => {
+        setTotal("1000");
+      });
+    setTotal(t);
+  };
 
-  // useEffect(() => {
-  //   getTotal();
+  useEffect(() => {
+    getTotal();
 
-  //   const intervalTotal = setInterval(() => {
-  //     getTotal();
-  //   }, 30000);
+    const intervalTotal = setInterval(() => {
+      getTotal();
+    }, 30000);
 
-  //   return () => {
-  //     clearInterval(intervalTotal);
-  //   };
-  // }, []);
+    return () => {
+      clearInterval(intervalTotal);
+    };
+  }, []);
 
   return (
     <Root>
@@ -102,7 +102,7 @@ export const Counter = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Airdrop Status: Running
+            Airdrop Stats
           </a>
         </p>
       </div>
